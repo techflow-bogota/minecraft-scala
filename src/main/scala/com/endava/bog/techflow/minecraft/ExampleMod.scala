bcb9ed10165a93e01439a247ae05fdb2a9b1ae29
Victor Mejia <vmejia@endava.com>
2018-06-06 15:29:20 -0500

    Base project to start moding minecraft in scala and Forge API

--- src/main/scala/com/endava/bog/techflow/minecraft/ExampleMod.scala
+++ src/main/scala/com/endava/bog/techflow/minecraft/ExampleMod.scala
@@ -2,21 +2,32 @@ package com.endava.bog.techflow.minecraft
 
 import net.minecraft.init.Blocks
 import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPreInitializationEvent, FMLStateEvent}
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent, FMLStateEvent}
 import org.apache.logging.log4j.Logger
 
-@Mod(modid = "scalamod", name = "Example Scala Mod", version = "1.0")
+@Mod(modid = "sm", name = "First Scala Mod", version = "1.0")
 class ExampleMod {
 
   var logger : Logger = null
 
+  @EventHandler
   def preInit(event: FMLPreInitializationEvent): Unit = {
+    //Block/Item initialization and registration
+    //Config handling
      logger = event.getModLog()
   }
 
+  @EventHandler
   def init(event: FMLInitializationEvent): Unit = {
+    //Proxy, TileEntity, entity, GUI Registration
 
     logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName())
+  }
+
+  @EventHandler
+  def postInit(event: FMLPostInitializationEvent): Unit = {
+
   }
 }
 
