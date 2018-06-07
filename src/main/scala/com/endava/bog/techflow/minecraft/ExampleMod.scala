2f878bafa7dd9dcef6770aac42c501146e0e6fa1
Alejandro Triana <alejandro.triana@outlook.com>
2018-06-07 04:44:17 -0500

    Add lifecycle events for Mod

--- src/main/scala/com/endava/bog/techflow/minecraft/ExampleMod.scala
+++ src/main/scala/com/endava/bog/techflow/minecraft/ExampleMod.scala
@@ -2,7 +2,8 @@ package com.endava.bog.techflow.minecraft
 
 import net.minecraft.init.Blocks
 import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPreInitializationEvent, FMLStateEvent}
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent, FMLStateEvent}
 import org.apache.logging.log4j.Logger
 
 @Mod(modid = "scalamod", name = "Example Scala Mod", version = "1.0")
@@ -10,13 +11,23 @@ class ExampleMod {
 
   var logger : Logger = null
 
+  @EventHandler
   def preInit(event: FMLPreInitializationEvent): Unit = {
+     //Item/Block init and registering
+     //Config Handling
      logger = event.getModLog()
+     logger.info("preInit() event")
   }
 
+  @EventHandler
   def init(event: FMLInitializationEvent): Unit = {
+    logger.info("init() event")
+  }
 
-    logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName())
+  @EventHandler
+  def postInit(event: FMLPostInitializationEvent): Unit = {
+    //list of register items, blocks etc
+    logger.info("postInit() event")
   }
 }
 
